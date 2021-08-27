a7c0d4b118f4715181e7c57d97ee1a9e4255b7d5
coliny125 <colinyuan3333@gmail.com>
2021-08-27 15:13:28 -0700

    Added large amts of ATM code

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,30 @@
+import java.util.*;
 
 public class ATM {
+	HashMap<String, Integer> map;
+	
+	public ATM() {
+		map = new HashMap<String, Integer>();
+	}
+	
+	public void deposit(String id, int amt) {
+		map.put(id, amt);
+//		Iterator mapIterator = map.entrySet().iterator();
+//		while (mapIterator.hasNext()) {
+//				
+//		}
+		
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "ATM [map=" + map + "]";
+	}
 
+	public static void main (String [] args) {
+		ATM chase = new ATM();
+		chase.deposit("COLINYUAN01", 1000000);
+		System.out.println(chase.toString());
+	}
 }
